4ddff8ecb227e83f2570906823c51b48c6b31a79
Mandeep Singh <mandeep88712@gmail.com>
2023-03-09 04:37:28 +0530

    UI update

--- FLIQ/settings.py
+++ FLIQ/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'rlgcthdg(6q#04xq&z$e2scu%m2()t2tz5zvy-#kp9k3a^%b%2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
