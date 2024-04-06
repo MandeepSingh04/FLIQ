864f674527bff6c100ac13cacb9ddf94ef7c5314
Mandeep Singh <mandeep88712@gmail.com>
2024-04-07 02:52:32 +0530

    render fix

--- FLIQ/asgi.py
+++ FLIQ/asgi.py
@@ -1,50 +1,14 @@
 """
 ASGI config for FLIQ project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
-# import os
-# from channels.auth import AuthMiddlewareStack
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from django.core.asgi import get_asgi_application
-# import chat.routing
-
-
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FLIQ.settings')
-
-# application = ProtocolTypeRouter( 
-#     {
-#         "http":get_asgi_application(),
-#         "websocket": AuthMiddlewareStack(
-#             URLRouter(
-#                chat.routing.websocket_urlpatterns
-#             )
-#         ),
-#     }
-# )
-
 import os
+
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FLIQ.settings')
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter , URLRouter
-from chat import routing
-
-django_asgi_app = get_asgi_application()
-application = ProtocolTypeRouter(
-	{
-		"http" : get_asgi_application() , 
-		"websocket" : AuthMiddlewareStack(
-			URLRouter(
-				routing.websocket_urlpatterns
-			) 
-		)
-	}
-)
+application = get_asgi_application()
